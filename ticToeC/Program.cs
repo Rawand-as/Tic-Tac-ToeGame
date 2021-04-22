cd60a16bf7152868c68f22f101705690d67f185f
Rawand abu salih <rawandas97@gmail.com>
2021-04-22 13:17:54 +0300

    changes

---
+++ ticToeC/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Threading;
+
+namespace ticToeC
+{
+    class Program
+    {
+
+        static int player = 1; //X is player 1 
+        static string flag = "None";//if flag x then x won if o then o won else Draw
+        static XOBoard board = null;
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\n");
+            Console.WriteLine("give me the size");
+            int size = int.Parse(Console.ReadLine());//get the size of the board
+
+            board = new XOBoard(size);
+            board.init();//init the board with dots
+          
+            Console.WriteLine("Player1:X and Player2:O");
+            do
+            {
+                //Console.Clear();
+                Console.WriteLine("\n");
+                if (player == 1)
+                {
+                    Console.WriteLine("X Turn\n");
+                    board.display();// calling the display Function  
+                    Console.WriteLine("give me row && col");
+                    int row = int.Parse(Console.ReadLine());
+                    int col = int.Parse(Console.ReadLine());
+
+                    if (row <= size && row > 0 && col <= size && col > 0)
+                    {
+                        if (board.checkPlace(row, col) == 1)
+                            Console.WriteLine("the place is already taken");
+                        else { 
+                            board.put(row, col, "x");
+                            player = 2;
+                        }
+                    }else
+                        Console.WriteLine("you have to put indecies from 1-" + size);
+                }
+                else if (player == 2)
+                {
+                    Console.WriteLine("O Turn\n");
+                    board.display();// calling the display Function  
+                    Console.WriteLine("give me row && col");
+                    int row = int.Parse(Console.ReadLine());
+                    int col = int.Parse(Console.ReadLine());
+
+                    if (row <= size && row > 0 && col <= size && col > 0)
+                    {
+                        if (board.checkPlace(row, col) == 1)
+                            Console.WriteLine("the place is already taken");
+                        else
+                        {
+                            board.put(row, col, "o");
+                            player = 1;
+                        }
+                    }
+                    else
+                        Console.WriteLine("you have to put indecies from 1-" + size);
+                }
+                flag = board.status();// calling status of game 
+            } while (flag != "x" && flag != "o" && flag != "Draw");
+
+           // Console.Clear();// clearing the console  
+            board.display();
+            if (flag == "x")
+            {
+                Console.WriteLine("The winner is X");
+            }
+            else if (flag == "o")
+            {
+                Console.WriteLine("The winner is O");
+            }
+            else if (flag == "Draw")
+            {
+                Console.WriteLine("The game ended with Draw");
+            }
+            else
+                Console.WriteLine("the game is still running");
+            Console.ReadLine();
+        } 
+    }
+}
---
+++ ticToeC/XOBoard.cs
@@ -0,0 +1,200 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ticToeC
+{
+    class XOBoard
+    {
+        
+        private int size = 0;
+        private string[,] board;
+
+        public XOBoard(int size)
+        {
+            this.size = size;
+            board = new string[size, size];
+        }
+
+        public void put(int row,int col,string choice)
+        { 
+            board[row - 1, col - 1] = choice;
+        }
+      
+     
+        public void init()
+        {
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < size; j++)
+                {
+                    board[i, j] = ".";
+                   
+                }
+            }
+        }
+
+        public string status()
+        {
+            bool isX = true;
+            bool isO = true;
+            bool isDraw = true;
+
+                //// Row  X to win
+                for (int i = 0; i < size; i++)
+                {
+                    isX = true;
+                    for (int j = 0; j < size; j++)
+                    {
+                        if (board[i, j] == "o" || board[i, j] == ".")//if at least one O ecist in the row then X won't win
+                        {
+                            isX = false;
+                        }
+                    }
+                    if (isX == true)
+                        return "x";
+                }
+
+
+
+                //Columns
+
+                for (int i = 0; i < size; i++)
+                {
+                    isX = true;
+                    for (int j = 0; j < size; j++)
+                    {
+                        if (board[j, i] == "o" || board[j, i] == ".")
+                        {
+                            isX = false;
+                        }
+                    }
+                    if (isX == true)
+                        return "x";
+                }
+
+
+
+            //Diagonals
+            //diagonal 1
+            for (int i = 0; i < size; i++)
+            {
+                isX = true;
+                for (int j = 0; j < size; j++)
+                    {
+                        if (board[j, j] == "o" || board[j, j] == ".")
+                            {
+                                isX = false;
+                            }
+                    }
+                    if (isX == true)
+                        return "x";
+            }
+            //diagonal 2
+            isX = true;
+            for (int j = size - 1, i = 0; j >= 0 && i < size; i++, j--)
+                {
+                    if (board[i, j] == "o" || board[i, j] == ".")
+                        isX = false;
+                }
+                if (isX == true)
+                    return "x";
+
+      
+
+                    //**********************************O Section
+
+                    //rowas for o winning
+                for (int i = 0; i < size; i++)
+                    {
+                        isO = true;
+                        for (int j = 0; j < size; j++)
+                        {
+                            if (board[i, j] == "x" || board[i, j] == ".")
+                            {
+                                isO = false;
+                            }
+                        }
+                        if (isO == true)
+                            return "o";
+                  }
+
+
+                //columns
+                for (int i = 0; i < size; i++)
+                {
+                    isO = true;
+                    for (int j = 0; j < size; j++)
+                    {
+                        if (board[j, i] == "x" || board[j, i] == ".")
+                        {
+                            isO = false;
+                        }
+
+                    }
+                    if (isO == true)
+                        return "o";
+                }
+
+
+            //diagnolas
+            for (int i = 0; i < size; i++)
+            {
+                isO = true;
+                for (int j = 0; j < size; j++)
+                    {
+                        if (board[j, j] == "x" || board[j, j] == ".")
+                            {
+                                isO = false;
+                            }
+                    }
+                    if (isO == true)
+                        return "o";
+
+            }
+            isO = true;
+            for (int j = size - 1, i = 0; j >= 0 && i < size; i++, j--)
+                {
+                    if (board[i, j] == "x" || board[i, j] == ".")
+                        isO = false;
+                }
+                if (isO == true)
+                    return "o";
+       
+
+
+            for (int i = 0; i < size; i++)//to check draw
+            {
+                for (int j = 0; j < size; j++)
+                {
+                    if (board[i, j] == ".")
+                        isDraw = false;
+                }
+            }
+            if (isDraw == true)
+                return "Draw";
+
+            return "None";
+        }
+
+        public int checkPlace(int row,int col)
+        {
+            if (board[row - 1, col - 1] != ".")
+                return 1;
+            return 0;
+        }
+
+        public void display()
+        {
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < size; j++)
+                {
+                    Console.Write(board[i, j]+"\t");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
+
